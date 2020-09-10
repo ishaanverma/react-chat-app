d5f8b7e787e6c436e8b2862e10801f625f4b0f97
Ishaan Verma <ishaanverma97@gmail.com>
2020-09-10 15:41:58 +0530

    added user online badge

--- frontend/src/components/ListWithText.js
+++ frontend/src/components/ListWithText.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -8,6 +9,14 @@ import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import Checkbox from '@material-ui/core/Checkbox';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import ListItemText from '@material-ui/core/ListItemText';
+import Badge from '@material-ui/core/Badge';
+
+const StyledBadge = withStyles((theme) => ({
+  badge: {
+    padding: '0 4px',
+    backgroundColor: '#44b700'
+  },
+}))(Badge);
 
 const ListWithText = ({ listData, click, data, icon, change, error }) => 
   <>
@@ -30,7 +39,17 @@ const ListWithText = ({ listData, click, data, icon, change, error }) =>
           return(
             <ListItem key={index} role={undefined} onClick={click(item)} dense button disableRipple>
               <ListItemAvatar>
-                <Avatar component={icon} />
+                <StyledBadge 
+                  invisible={listData.onlineList[item.id] === 'online' ? false : true}
+                  overlap="circle"
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                  }}
+                  variant="dot"
+                >
+                  <Avatar component={icon} />
+                </StyledBadge>
               </ListItemAvatar>
               <ListItemText primary={item.name} secondary={listData.onlineList[item.id] === 'online' ? 'online' : 'offline'} />
               <ListItemSecondaryAction>
